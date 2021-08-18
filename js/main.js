0e1b09a622d55770ce847321875dddffa09017a9
DavidLugo847 <yeisonacabrerab@gmail.com>
2021-08-18 16:34:23 -0500

    Agregando menu responsive 2

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+
+    $('.menu-bar').click(function(){
+
+        if(contador == 1){
+
+            $('nav').animate({
+
+                left:0
+            });
+            contador = 0;
+        }
+        else{
+            
+            contador = 1;
+
+        $('nav').animate({
+            left:'-100%'
+        });
+    }
+    });
+
+    $('.submenu').click(function(){
+        $(this).children('.children').slideToggle();
+    });
+}
