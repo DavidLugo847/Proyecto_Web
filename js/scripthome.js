f80cf6e5642aeca61f535bbc1e0a8f3183dc5bc5
DavidLugo847 <krt847@gmail.com>
2022-02-18 02:45:22 -0500

    Modificando el repositorio al estilo de Win95

---
+++ js/scripthome.js
@@ -0,0 +1,45 @@
+function underline1(id){
+    document.getElementById(id).className ="uline";
+}
+function underline2(id){
+    document.getElementById(id).className="uline";
+}
+function underline3(id){
+    document.getElementById(id).className="uline";
+}
+function underline4(id){
+    document.getElementById(id).className="uline";
+}
+function underline5(id){
+    document.getElementById(id).className="uline";
+}
+function underline6(id){
+    document.getElementById(id).className="uline";
+}
+function underline7(id){
+    document.getElementById(id).className="uline";
+}
+
+// Unline
+
+function unline1(id){
+    document.getElementById(id).className ="unline";
+}
+function unline2(id){
+    document.getElementById(id).className="unline";
+}
+function unline3(id){
+    document.getElementById(id).className="unline";
+}
+function unline4(id){
+    document.getElementById(id).className="unline";
+}
+function unline5(id){
+    document.getElementById(id).className="unline";
+}
+function unline6(id){
+    document.getElementById(id).className="unline";
+}
+function unline7(id){
+    document.getElementById(id).className="unline";
+}
